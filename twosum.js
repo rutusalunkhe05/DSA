6d4d31c194f4040cd79c6b1e9d13db1e012a4780
rutusalunkhe05 <ritusalunkhe05@gmail.com>
2024-05-23 15:06:45 +0530

    Add files via upload

---
+++ twosum.js
@@ -0,0 +1,17 @@
+var nums = [4,7,25,11];
+
+var target = 18;
+
+function twoSum(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      var addition = nums[i] + nums[j];
+      if (target === addition) {
+
+        return [i, j];
+      }
+    }
+  }
+}
+var output = twoSum(nums, target);
+console.log(output);
